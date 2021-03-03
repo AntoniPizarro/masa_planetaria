b4783e5dbf968cb31d6d8d9615e4ee6caf0ae6a5
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-03-03 23:50:47 +0100

    Main

--- planets/src/main/java/edu/pingpong/planets/Planets.java
+++ planets/src/main/java/edu/pingpong/planets/Planets.java
@@ -1,11 +1,24 @@
 package edu.pingpong.planets;
 
-/**
- * Hello world!
- *
- */
+import edu.pingpong.planets.Valores;
+
 public class Planets {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+		 
+        double peso = 175.0; // kg
+        
+        for(Valores planeta : Valores.values()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+        
+        System.out.println("\nYour weight only on the terrestrial planets: ");
+        for(Valores planeta: Valores.getPlanetasRocosos()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+        
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for(Valores planeta: Valores.getGigantesGaseosos()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
     }
 }
